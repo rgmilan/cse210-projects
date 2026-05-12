9c0f8a79f807429ccd3e9fcb5977b0637e7f59b5
rgmilan <dj.rg.milan@gmail.com>
2026-05-12 23:02:49 +0800

    W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Construction Manager";
+        job1._company = "Kele Built";
+        job1._startYear = "2017";
+        job1._endYear = "2022";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Construction Manager";
+        job2._company = "Delectable Ventures Inc";
+        job2._startYear = "2022";
+        job2._endYear = "Present";
+
+        Resume myResume = new Resume();
+        myResume._name = "Argy Milan";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
