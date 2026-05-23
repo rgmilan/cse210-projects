269b9ce7ab9a85f700b7133b6388dafbf3e58c9f
rgmilan <dj.rg.milan@gmail.com>
2026-05-23 13:07:08 +0800

    Scriptiure Memorizer - Week 03

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Mosiah", 2, 17);
+
+        string fullScriptureText = "When ye are in the service of your fellow beings ye are only in the service of your God";
+
+        Scripture scripture = new Scripture(reference, fullScriptureText);
+
+        string userInput = "";
+
+        while (userInput != "quit" && !scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue or type 'quit' to end:");
+
+            userInput = Console.ReadLine();
+
+            if (userInput != "quit")
+            {
+                scripture.HideRandomWords(3);
+            }
+        }
+
+        if (userInput != "quit")
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine();
+            Console.WriteLine("All words are now hidden.");
+            Console.WriteLine("You have 1 last try to recite it from memory.");
+            Console.WriteLine("Press enter to see the complete scripture one last time.");
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine("Complete scripture:");
+            Console.WriteLine($"{reference.GetDisplayText()} {fullScriptureText}");
+        }
     }
 }
