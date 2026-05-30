14c28fd2fb5db68274bbea65276c5dd918673cd1
rgmilan <dj.rg.milan@gmail.com>
2026-05-30 16:57:09 +0800

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Base Assignment
+        Assignment a1 = new Assignment(
+            "Argy Milan",
+            "Construction Estimating"
+        );
+
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        // Math Assignment
+        MathAssignment a2 = new MathAssignment(
+            "Argy Milan",
+            "Concrete Works",
+            "5.2",
+            "1-12"
+        );
+
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        // Writing Assignment
+        WritingAssignment a3 = new WritingAssignment(
+            "Argy Milan",
+            "Project Management",
+            "Construction Site Safety Procedures"
+        );
+
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
