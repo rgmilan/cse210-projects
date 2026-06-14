500cde9183f0cef956e6ab99cac022ddaac2c66d
rgmilan <dj.rg.milan@gmail.com>
2026-06-14 10:03:33 +0800

    W07 Assignment: Exercise Tracking Program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         // Exceeded Requirements:
-        // Added an automatic save feature. Whenever the user exits the
-        // program using the Quit option, all goals and the current score
-        // are automatically saved to autosave.txt. This helps prevent
-        // accidental data loss if the user forgets to manually save.
+        // I added an autosave feature to the program. When the user chooses
+        // the Quit option, the program automatically saves all goals and the
+        // current score to autosave.txt before closing. This way, the user
+        // won't lose progress if they forget to save manually.
 
         GoalManager goalManager = new GoalManager();
         goalManager.Start();
